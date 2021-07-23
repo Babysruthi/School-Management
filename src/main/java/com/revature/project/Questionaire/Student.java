64cc5d5caa58f62a86c16a2956a5ca8a2eae5242
Babysruthi <76945582+Babysruthi@users.noreply.github.com>
2021-07-23 20:19:57 +0530

    Project created

---
+++ src/main/java/com/revature/project/Questionaire/Student.java
@@ -0,0 +1,21 @@
+package com.revature.project.Questionaire;
+
+public class Student 
+{
+	Integer studentRollNo;
+	String studentName;
+	String studentDob;
+	String studentStd;
+	String studentAddress;
+	public Student(Integer studentRollNo, String studentName, String studentDob, String studentStd,
+			String studentAddress) {
+		super();
+		this.studentRollNo = studentRollNo;
+		this.studentName = studentName;
+		this.studentDob = studentDob;
+		this.studentStd = studentStd;
+		this.studentAddress = studentAddress;
+	}
+	
+	
+}
