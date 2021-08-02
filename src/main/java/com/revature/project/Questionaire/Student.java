c6fd2a5ab2c90854309d32081316f4340581bc53
Babysruthi <76945582+Babysruthi@users.noreply.github.com>
2021-08-02 12:29:20 +0530

    -first commit

--- src/main/java/com/revature/project/Questionaire/Student.java
+++ src/main/java/com/revature/project/Questionaire/Student.java
@@ -1,21 +1,50 @@
 package com.revature.project.Questionaire;
 
-public class Student 
-{
-	Integer studentRollNo;
-	String studentName;
-	String studentDob;
-	String studentStd;
-	String studentAddress;
-	public Student(Integer studentRollNo, String studentName, String studentDob, String studentStd,
-			String studentAddress) {
-		super();
-		this.studentRollNo = studentRollNo;
-		this.studentName = studentName;
-		this.studentDob = studentDob;
-		this.studentStd = studentStd;
-		this.studentAddress = studentAddress;
+public class Student {
+	private Integer rollNo;
+	private String name;
+	private String dob;
+	private String standard;
+	private String address;
+	private int classNo;
+	
+	public int getClassNo() {
+		return classNo;
 	}
+	public void setClassNo(int classNo) {
+		this.classNo = classNo;
+	}
+	public Integer getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(Integer rollNo) {
+		this.rollNo = rollNo;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getStandard() {
+		return standard;
+	}
+	public void setStandard(String standard) {
+		this.standard = standard;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 	
-	
+
 }
